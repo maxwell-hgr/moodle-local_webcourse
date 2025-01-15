988c7fb8b095054b07777b28272d0abf286b6254
maxwell-hgr <maxwell.hygor01@gmail.com>
2025-01-15 10:42:29 -0300

    Version correction

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_webcourse';
-$plugin->version = 2024121200;
+$plugin->version = 2025011500;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.0';
